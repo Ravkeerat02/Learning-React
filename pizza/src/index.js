40b0c0310b9cb91f76890d0516db2f225fc90b7e
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-08-04 03:08:12 -0400

    playign with props

--- pizza/src/index.js
+++ pizza/src/index.js
@@ -58,21 +58,10 @@ function App() {
   );
 }
 
-//rules - name in CAPS - needs to return JSX
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Spianchi"></img>
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
-}
-
 function Header() {
   return (
     <header className="header">
-      <h3>My Pizza Place</h3>
+      <h1>My Pizza Place</h1>
     </header>
   );
 }
@@ -81,18 +70,50 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spianci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        photoName="pizzas/funghi.jpg"
+        price={12}
+      />
+      <Pizza
+        name="Pizza Salamino"
+        ingredients="Tomato, mozarella, and pepperoni"
+        photoName="pizzas/salamino.jpg"
+        price={15}
+      />
+      <Pizza
+        name="Pizza Prosciutto"
+        ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+        photoName="pizzas/prosciutto.jpg"
+        price={18}
+      />
     </main>
+  );
+}
+//rules - name in CAPS - needs to return JSX
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name}></img>
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
   );
 }
 
 function Footer() {
   //return React.createElement("footer", null, "We are open");
+  // adding some condtional styling to footer - green if open - red if closed
   const hour = new Date().getHours();
   const openHour = 12;
   const closeHour = 22;
@@ -104,7 +125,7 @@ function Footer() {
   return (
     <div>
       <footer className="footer">
-        {new Date().toLocaleTimeString()}We are open
+        {new Date().toLocaleTimeString()} We are open
       </footer>
     </div>
   );
