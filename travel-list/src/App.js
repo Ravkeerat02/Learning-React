e6907282761ac1656605778dbbb41c9f16762844
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-08-10 03:31:57 -0400

    Controlled elements

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -20,24 +22,42 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   // setting up event handler
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e);
+
+    // guard clasue
+    if (!description) return;
+
+    const newItem = { id: Date.now(), description, quantity, packed: false };
+    console.log(newItem);
+
+    // reset form
+    setDescription("");
+    setQuantity(1);
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you neecd for your trip 🛣️ trip ?</h3>
-      {/* setting up form */}
-      <select>
-        {/* creating dynamic list  */}
+      <h3>What do you need for your trip 🛣️?</h3>
+      {/* Setting up form */}
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+        {/* Creating dynamic list */}
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item...." />
+      <input
+        type="text"
+        placeholder="Item...."
+        value={description}
+        // Reads the value of the input field
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
