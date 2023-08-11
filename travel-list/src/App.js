6a4d75d00d3890e0025be020f9025a66fd035cf8
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-08-11 01:49:06 -0400

    sorting

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -97,10 +97,23 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleitems }) {
+  const [sortBy, setSortBy] = useState("input");
+  // for 3 different conditions
+  let sortedItems;
+  // sorting by input
+  if (sortBy === "input") sortedItems = items;
+  // sorting alphabetically
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  // sorting by packing status
+  if (sortBy === "packed")
+    sortedItems = items.slice().sort((a, b) => a.packed - b.packed);
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             onDeleteItem={onDeleteItem}
@@ -109,6 +122,16 @@ function PackingList({ items, onDeleteItem, onToggleitems }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          {/* order by which they were placed */}
+          <option value="input">Sort by Input Order</option>
+          {/* sort by description */}
+          <option value="description">Sort by Description</option>
+          {/* sort by packing status */}
+          <option value="packed">Sort by Packing Status</option>
+        </select>
+      </div>
     </div>
   );
 }
